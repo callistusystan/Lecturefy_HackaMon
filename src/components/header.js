c7c0a9338e65625f94796d478be5243411a5bfd4
callistusystan <callistusystan@gmail.com>
2017-03-25 13:31:20 +1100

    Restructured the routers

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default (props) => {
 	return (
-    <div>
-    Hello World
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to Pineapple JS</h2>
+      </div>
+      <div>
+        {props.children}
+      </div>
     </div>
   );
 };
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { ButtonToolbar, Button } from 'react-bootstrap';
 
+import Header from './header';
+
 class HomePage extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const containerStyle = {margin: '10px auto 10px'};
     return (
-      <ButtonToolbar>
-        <Button bsStyle="primary">Login as Presenter</Button>
-        <Button bsStyle="primary">Login as Viewer</Button>
-      </ButtonToolbar>
+      <div>
+        <Header />
+
+        <div className="container-fluid" style={containerStyle}>
+          <ButtonToolbar>
+            <Button bsStyle="primary">Login as Presenter</Button>
+            <Button bsStyle="primary">Login as Viewer</Button>
+          </ButtonToolbar>
+        </div>
+      </div>
     );
   }
 }
