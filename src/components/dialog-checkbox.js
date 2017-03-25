8943878080dccfb75e3bc4ef9dde20173f621359
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-03-26 02:26:58 +1100

    Added aws-sdk

--- src/components/dialog-checkbox.js
+++ src/components/dialog-checkbox.js
@@ -1,36 +1,72 @@
 import React from 'react';
-import io from 'socket.io';
+import SocketIO from 'socket.io-client';
+
+function findAnswer(searched_id, question) {
+    let selected_answer = null;
+    question.answers.some((answer)=>{
+        let found_answer = answer.id == searched_id;
+        if(found_answer) {
+            selected_answer = answer;
+        }
+        return found_answer;
+    })
+    return selected_answer;
+}
 class PollDialog extends React.Component {
     constructor(props) {
         super(props);
-        var socket = new io.Socket();
-        this.props.io = socket.connect('http://127.0.0.1:8080');
+        const parent = this;
         let question = {
-            answers: [{description: "yes"}, {description:"definately yes"}]
+            id: 0,
+            answers: [
+                {id: 1, description: "yes", poll_count: 0},
+                {id: 2, description: "definately yes", poll_count: 0}
+            ]
         };
-        this.state = {
+        parent.state = {
             question: question
         };
-        io.sockets.emit("rawr!", {value: 0});
-        io.sockets.on('rawr', function(json) {
-            console.log(json);
-            io.sockets.emit("rawr", {value: json.value + 1});
+        // SOCKET IO SHOT======
+        this.socket = SocketIO('http://localhost:1337');
+        this.socket.on('answer_update', (partial_answer_data)=> {
+            this.onAnswerUpdate(partial_answer_data);
         })
     }
+    onAnswerUpdate(partial_answer_data) {
+        console.log(partial_answer_data);
+        let selected_answer = findAnswer(partial_answer_data.answer.id, this.state.question);
+        if(selected_answer == null) {
+            console.log(`Answer with id ${partial_answer_data.answer.id} was null`);
+            return;
+        }
+        selected_answer.poll_count = partial_answer_data.answer.poll_count
+        this.setState({
+          question: this.state.question
+        });
+    }
+
+    onSelectOption(item, event) {
+        console.log(item);
+        let username = "test"; //this.props.username;
+        this.socket.emit('answer_question', {username: username, question_id: this.state.question.id, answer_id: item.id});
+    }
+
     render() {
         const parent = this;
         return (
             <section className="poll-container">
                 <h1>Rawr</h1>
+                <form className="form-group">
                 {
                     parent.state.question.answers.map((item, index)=> {
                         let checkboxName = `chk-${index}`;
-                        return <section key={index}>
-                            <input type="checkbox" name={checkboxName}/>
+                        return <section key={item.id}>
+                            <input type="radio" name={checkboxName} checked={this.state.selectedOption == index} onChange={this.onSelectOption.bind(this, item)}/>
                             <label htmlFor={checkboxName}>{item.description}</label>
                         </section>
                     })
                 }
+                </form>
             </section>
         );
     }
