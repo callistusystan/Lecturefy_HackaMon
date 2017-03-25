5d43498e3cd15f57e0d8625774928f3b4333d4aa
callistusystan <callistusystan@gmail.com>
2017-03-25 13:31:52 +1100

    changes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import Header from './components/header';
+import App from './App';
 import Presentation from './components/presentation';
 import HomePage from './components/homepage';
 import AuthcatePage from './components/authcate-page.js';
 export default (
     <Route path="/" >
         <IndexRoute component={HomePage} />
-        <Route path="presentation" component={Header} >
-          <IndexRoute component={Presentation} />
-        </Route>
-        <Route path="login/" component={AuthcatePage}/>
+        <Route path="presentation" component={Presentation} />
+        <Route path="login" component={AuthcatePage}/>
     </Route>
 );
