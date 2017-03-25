fb419d68c16b772a3865f8ba2603b784d036bfdc
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-03-25 16:56:40 +1100

    Added more stuff

---
+++ src/components/homepage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ButtonGroup,ButtonToolbar, Button, Jumbotron } from 'react-bootstrap';
+
+import Header from './header';
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const containerStyle = {margin: 'auto'};
+    return (
+      <div>
+        <Header />
+            <div className="buttonContainer">
+                <Button bsStyle="primary" bsSize="large" className="center-button">Login as Presenter</Button>
+                <Button bsStyle="primary" bsSize="large" className="center-button">Login as Viewer</Button>
+            </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
