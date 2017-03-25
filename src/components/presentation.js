e47691f37fcc2aef958db157c55e93d48db8aee3
callistusystan <callistusystan@gmail.com>
2017-03-26 03:11:45 +1100

    updated viewer view

--- src/components/presentation.js
+++ src/components/presentation.js
@@ -3,36 +3,44 @@ import { Button, Modal, Jumbotron, Grid, Row, Col } from 'react-bootstrap';
 
 import SlideView from './slideView';
 import BoosterView from './boosterView';
-import SocketIO from 'socket.io';
+import SocketIO from 'socket.io-client';
 const containerStyle = { margin: '0% 5%', height: '100%', minHeight: '100%' };
 
 class Presentation extends Component {
   constructor(props) {
     super(props);
-    this.socket = SocketIO('http://localhost:1337');
+    this.socket = SocketIO('http://118.138.41.192:1337');
     this.state = {
-      isPresenter: false
+      isPresenter: false,
+      hasEvent: true
     };
   }
 
-  renderPollModal() {
-    // modal
+  renderPollView() {
     return (
-      <Modal.Dialog>
-        <h2>Do you think Pineapple Bae should win Hackamon?</h2>
-        <Button>Yes</Button>
-        <br />
-        <Button>DefinitelyYes</Button>
-      </Modal.Dialog>
+      <Col xs={4} md={4}>
+        <Jumbotron>
+          <h2>Do you think Pineapple Bae should win Hackamon?</h2>
+          <Button onClick={this.onYesClick.bind(this)} style={{width: '200px'}} bsStyle='primary' bsSize="large">Yes!!!</Button>
+          <br />
+          <Button onClick={this.onDefinitelyYesClick.bind(this)} style={{marginTop: '10px', width: '200px'}} bsStyle='success' bsSize="large">Definitely Yes!!!</Button>
+        </Jumbotron>
+      </Col>
     );
   }
 
   onYesClick() {
     // emit yes clicked
+    this.setState({
+      hasEvent: false
+    });
   }
 
   onDefinitelyYesClick() {
     // emit definitely yes clicked
+    this.setState({
+      hasEvent: false
+    });
   }
 
   componentWillMount() {
@@ -63,12 +71,21 @@ class Presentation extends Component {
   }
 
   renderViewerView() {
+    if (this.state.hasEvent) {
+      return (
+        <Grid fluid={true} style={containerStyle}>
+          <Row>
+            <Col xs={7} md={7}><SlideView socket={this.socket} isPresenter={this.state.isPresenter} /></Col>
+            <Col xs={1} md={1} />
+            {this.renderPollView()}
+          </Row>
+        </Grid>
+      );
+    }
     return (
       <Grid fluid={true} style={containerStyle}>
         <Row>
-          <Col xs={7} md={7}><SlideView socket={this.socket} isPresenter={this.state.isPresenter} /></Col>
-          <Col xs={1} md={1} />
-          <Col xs={4} md={4}>{this.renderPollModal()}</Col>
+          <Col style={{float: 'none', margin: '0 auto'}} xs={7} md={7}><SlideView socket={this.socket} isPresenter={this.state.isPresenter} /></Col>
         </Row>
       </Grid>
     );
