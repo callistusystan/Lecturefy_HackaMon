9fc4dcb590c1b4969d1c10267dce70809afa1832
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-03-25 17:12:43 +1100

    Added yarn lock and logo-monash-minimal

--- src/components/authcate-page.js
+++ src/components/authcate-page.js
@@ -1,21 +1,53 @@
 import React from 'react';
 import LogoMonash from '../drawables/logo-monash.svg';
-import BackgroundMonashAuthcate from '../drawables/background-monash-authcate.png';
+import BackgroundMonashAuthcate from '../drawables/background-authcate-monash.png';
+import '../css/authcate-page.css';
+import {Link} from 'react-router';
 class AuthcatePage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+        this.onUsernameChange = this.onUsernameChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+    }
+    onUsernameChange(event) {
+        this.setState({username: event.target.value});
+    }
+    onPasswordChange(event) {
+        this.setState({password: event.target.value});
     }
     render() {
         return (
-            <div className="container-fluid">
-                <section className="col-md-8">
-                    <img src={BackgroundMonashAuthcate}/>
-                </section>
-                <form className="input-group col-md-4">
-                    <input type="text" class="form-control" placeholder="Username"/>
-                    <input type="text" class="form-control" placeholder="Password"/>
-                    <h1>Callistus Tan the God of Dank Memes</h1>
-                </form>
+            <div className="container-fluid authcate-container h-match-parent">
+                <div className="row h-match-parent">
+                    <section className="col-md-7 col-sm-6 col-xs-0 authcate-side-hero">
+                        <div className="row">
+                            <img className="h-match-parent w-match-parent" src={BackgroundMonashAuthcate}/>
+                        </div>
+                    </section>
+                    <div className="col-md-5 col-sm-6 col-xs-12 authcate-sidebar">
+                        <div>
+                            <img className="auth-monash-logo" src={LogoMonash}/>
+                            <p className="padding-md-v">Sign in with your Monash email address</p>
+                            <form className="form-group">
+                                <div className="input-group w-match-parent input-authcate">
+                                    <input type="text" className="form-control" placeholder="Username" onChange={this.onChangeUsernmae}/>
+                                </div>
+                                <div className="input-group w-match-parent">
+                                    <input type="text" className="form-control" placeholder="Password" onChange={this.onChangePassword}/>
+                                </div>
+                                <Link to={{pathname:"/presentation",  query:{username: this.state.username, password: this.state.password}}}>
+                                    <button type="button" className="btn bt-lg btn-sign-up">Sign up</button>
+                                </Link>
+                            </form>
+                            <p><a href="https://google.com" className="text-link-colored">Change Password</a></p>
+                            <p><a href="https://google.com" className="text-link-colored">Forgot your Password?</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
