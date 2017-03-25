efc6e98f6da51fd0ca0d9ed557462c8d1d6e2fb8
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-03-25 12:51:04 +1100

    Added authcate page to React-Router

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,10 @@ import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import Presentation from './components/presentation';
 import HomePage from './components/homepage';
-
+import AuthcatePage from './components/authcate-page.js';
 export default (
     <Route path="/" component={App} >
         <IndexRoute component={HomePage} />
+        <Route path="login/" component={AuthcatePage}/>
     </Route>
 );
