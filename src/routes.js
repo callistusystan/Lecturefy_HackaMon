cd7b30268ff8f1cb8fa48f93906a9d0c334abd59
callistusystan <callistusystan@gmail.com>
2017-03-25 14:01:10 +1100

    commit routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import App from './App';
 import Presentation from './components/presentation';
 import HomePage from './components/homepage';
 import AuthcatePage from './components/authcate-page.js';
+
+import Header from './components/header';
+
 export default (
     <Route path="/" >
         <IndexRoute component={HomePage} />
-        <Route path="presentation" component={Presentation} />
+        <Route path="presentation" component={Header}>
+          <IndexRoute component={Presentation} />
+        </Route>
         <Route path="login" component={AuthcatePage}/>
     </Route>
 );
