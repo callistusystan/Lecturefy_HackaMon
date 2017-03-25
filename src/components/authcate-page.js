3081d1b2466282d11c0743690d79394caeb6cc78
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-03-25 19:24:32 +1100

    Fixed method typo in authcate-page

--- src/components/authcate-page.js
+++ src/components/authcate-page.js
@@ -14,12 +14,14 @@ class AuthcatePage extends React.Component {
         this.onPasswordChange = this.onPasswordChange.bind(this);
     }
     onUsernameChange(event) {
+        console.log(event.target.value);
         this.setState({username: event.target.value});
     }
     onPasswordChange(event) {
         this.setState({password: event.target.value});
     }
     render() {
+        const parent = this;
         return (
             <div className="container-fluid authcate-container h-match-parent">
                 <div className="row h-match-parent">
@@ -34,13 +36,13 @@ class AuthcatePage extends React.Component {
                             <p className="padding-md-v">Sign in with your Monash email address</p>
                             <form className="form-group">
                                 <div className="input-group w-match-parent input-authcate">
-                                    <input type="text" className="form-control" placeholder="Username" onChange={this.onChangeUsernmae}/>
+                                    <input type="text" className="form-control" placeholder="Username" onChange={parent.onUsernameChange}/>
                                 </div>
                                 <div className="input-group w-match-parent">
-                                    <input type="text" className="form-control" placeholder="Password" onChange={this.onChangePassword}/>
+                                    <input type="text" className="form-control" placeholder="Password" onChange={parent.onPasswordChange}/>
                                 </div>
-                                <Link to={{pathname:"/presentation",  query:{username: this.state.username, password: this.state.password}}}>
-                                    <button type="button" className="btn bt-lg btn-sign-up">Sign up</button>
+                                <Link to={{pathname:"/presentation",  query:{username: parent.state.username}}}>
+                                    <button type="button" className="btn bt-lg btn-sign-up">Sign in</button>
                                 </Link>
                             </form>
                             <p><a href="https://google.com" className="text-link-colored">Change Password</a></p>
