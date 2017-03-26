078097059113c24860a3299759e23534db22e3cb
callistusystan <callistusystan@gmail.com>
2017-03-26 12:07:32 +1100

    updated slides

--- src/components/slideView.js
+++ src/components/slideView.js
@@ -7,7 +7,7 @@ class SlideView extends Component {
 
 		this.state = {
 			curIndex: 0,
-			images: ["images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg","images/6.jpg","images/7.jpg","images/8.jpg"]
+			images: ["images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg","images/6.jpg","images/7.jpg","images/8.jpg","images/9.jpg"]
 		};
 		this.props.socket.on('onPresenterSlideIndexChanged', (slide_information) => {
 			this.onPresenterSlideIndexChanged(slide_information);
